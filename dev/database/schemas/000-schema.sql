519a60d9b90b092a86f4cc1a43fff9e8e575368b
Pedro Ribeiro <pedroalgoribeiro@gmail.com>
2025-04-05 14:10:41 +0100

    Adds basic docker and database setup

---
+++ dev/database/schemas/000-schema.sql
@@ -0,0 +1,8 @@
+-- Create the GOLANG_TEMPLATE schema
+CREATE SCHEMA IF NOT EXISTS GOLANG_TEMPLATE;
+
+-- Set the search path to use the GOLANG_TEMPLATE schema
+SET search_path TO GOLANG_TEMPLATE;
+
+-- Add a comment to the schema
+COMMENT ON SCHEMA GOLANG_TEMPLATE IS 'Schema for the Golang Template project';
